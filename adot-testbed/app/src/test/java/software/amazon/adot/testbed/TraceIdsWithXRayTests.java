4c89fb0a0e6703ed58bde348f145202364f52985
Jonathan Lee <jjllee@amazon.com>
2023-10-18 15:23:53 -0700

    remove unused imports

--- adot-testbed/app/src/test/java/software/amazon/adot/testbed/TraceIdsWithXRayTests.java
+++ adot-testbed/app/src/test/java/software/amazon/adot/testbed/TraceIdsWithXRayTests.java
@@ -1,40 +1,19 @@
 package software.amazon.adot.testbed;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.PrintWriter;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.nio.file.Path;
 import java.nio.file.Files;
-import java.net.URISyntaxException;
-import java.time.Duration;
-import java.time.Instant;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.rholder.retry.RetryerBuilder;
-import com.github.rholder.retry.StopStrategies;
-import com.github.rholder.retry.WaitStrategies;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.TestInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,36 +25,24 @@ import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.MountableFile;
-import software.amazon.awssdk.services.cloudwatchlogs.CloudWatchLogsClient;
-import software.amazon.awssdk.services.cloudwatchlogs.model.GetLogEventsRequest;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
-import io.opentelemetry.context.propagation.ContextPropagators;
-import io.opentelemetry.sdk.OpenTelemetrySdk;
-import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
-import io.opentelemetry.sdk.resources.Resource;
-import io.opentelemetry.sdk.trace.SdkTracerProvider;
-import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
-import io.opentelemetry.sdk.logs.SdkLoggerProvider;
-import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
-import io.opentelemetry.sdk.logs.export.LogRecordExporter;
+import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.sdk.trace.samplers.Sampler;
-
-import io.opentelemetry.api.GlobalOpenTelemetry;
-
-import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
-import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
-
 import io.opentelemetry.contrib.awsxray.AwsXrayIdGenerator;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.sdk.trace.samplers.Sampler;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.regions.Region;
@@ -86,20 +53,22 @@ import software.amazon.awssdk.services.xray.model.BatchGetTracesResponse;
 // @Testcontainers(disabledWithoutDocker = true)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class TraceIdsWithXRayTests {
-    private static final String TEST_IMAGE = System.getenv("TEST_IMAGE") != null && !System.getenv("TEST_IMAGE").isEmpty()
-        ? System.getenv("TEST_IMAGE")
-        : "public.ecr.aws/aws-observability/aws-otel-collector:latest";
+    private static final String TEST_IMAGE = System.getenv("TEST_IMAGE") != null
+            && !System.getenv("TEST_IMAGE").isEmpty()
+                    ? System.getenv("TEST_IMAGE")
+                    : "public.ecr.aws/aws-observability/aws-otel-collector:latest";
     private final Logger collectorLogger = LoggerFactory.getLogger("collector");
     private static final String uniqueID = UUID.randomUUID().toString();
     private Path logDirectory;
     private GenericContainer<?> collector;
 
-    private GenericContainer<?> createAndStartCollector(String configFilePath, String logStreamName) throws IOException {
+    private GenericContainer<?> createAndStartCollector(String configFilePath, String logStreamName)
+            throws IOException {
 
         // Create an environment variable map
         Map<String, String> envVariables = new HashMap<>();
         envVariables.put("LOG_STREAM_NAME", logStreamName);
-        //Set credentials
+        // Set credentials
         envVariables.put("AWS_REGION", System.getenv("AWS_REGION"));
         envVariables.put("AWS_ACCESS_KEY_ID", System.getenv("AWS_ACCESS_KEY_ID"));
         envVariables.put("AWS_SECRET_ACCESS_KEY", System.getenv("AWS_SECRET_ACCESS_KEY"));
@@ -114,30 +83,30 @@ class TraceIdsWithXRayTests {
             throw new RuntimeException("Failed to create log directory", e);
         }
         var collector = new FixedHostPortGenericContainer<>(TEST_IMAGE)
-            .withCopyFileToContainer(MountableFile.forClasspathResource(configFilePath), "/etc/collector/config.yaml")
-            .withFixedExposedPort(4317, 4317, InternetProtocol.TCP)
-            .withLogConsumer(new Slf4jLogConsumer(collectorLogger))
-            .waitingFor(Wait.forLogMessage(".*Everything is ready. Begin running and processing data.*", 1))
-            .withEnv(envVariables)
-            .withClasspathResourceMapping("/logs", "/logs", BindMode.READ_WRITE)
-            .withCommand(
-                "--config", "/etc/collector/config.yaml",
-                "AWS_REGION=us-west-2"
-            );
+                .withCopyFileToContainer(MountableFile.forClasspathResource(configFilePath),
+                        "/etc/collector/config.yaml")
+                .withFixedExposedPort(4317, 4317, InternetProtocol.TCP)
+                .withLogConsumer(new Slf4jLogConsumer(collectorLogger))
+                .waitingFor(Wait.forLogMessage(".*Everything is ready. Begin running and processing data.*", 1))
+                .withEnv(envVariables)
+                .withClasspathResourceMapping("/logs", "/logs", BindMode.READ_WRITE)
+                .withCommand(
+                        "--config", "/etc/collector/config.yaml",
+                        "AWS_REGION=us-west-2");
 
-        collector.withFileSystemBind(logDirectory.toString(),"/tempLogs", BindMode.READ_WRITE);
+        collector.withFileSystemBind(logDirectory.toString(), "/tempLogs", BindMode.READ_WRITE);
         collector.start();
         return collector;
     }
 
     @Test
     void testXRayTraceIdSendToXRay() throws Exception {
-        sendTracesToXRayHelper(false);
+        sendTracesToXRayHelper(true);
     }
 
     @Test
     void testW3CTraceIdSendToXRay() throws Exception {
-        sendTracesToXRayHelper(true);
+        sendTracesToXRayHelper(false);
     }
 
     void sendTracesToXRayHelper(boolean useXRayIDGenerator) throws Exception {
@@ -149,18 +118,17 @@ class TraceIdsWithXRayTests {
         Tracer tracer = otel.getTracer("xray-w3c-id-test");
 
         Attributes attributes = Attributes.of(
-            AttributeKey.stringKey("http.method"), "GET",
-            AttributeKey.stringKey("http.url"), "http://localhost:8080/randomEndpoint"
-        );
+                AttributeKey.stringKey("http.method"), "GET",
+                AttributeKey.stringKey("http.url"), "http://localhost:8080/randomEndpoint");
 
         int numOfTraces = 5;
         List<String> traceIds = new ArrayList<String>();
-        HashSet<String> traceIdsSet = new HashSet<String>(); 
+        Set<String> traceIdsSet = new HashSet<String>();
         for (int count = 0; count < numOfTraces; count++) {
             Span span = tracer.spanBuilder("trace-span-test")
-                .setSpanKind(SpanKind.SERVER)
-                .setAllAttributes(attributes)
-                .startSpan();
+                    .setSpanKind(SpanKind.SERVER)
+                    .setAllAttributes(attributes)
+                    .startSpan();
             System.out.print("Created Span #" + count + " - ");
             String id = new StringBuilder(span.getSpanContext().getTraceId()).insert(8, "-").insert(0, "1-").toString();
             traceIds.add(id);
@@ -175,57 +143,61 @@ class TraceIdsWithXRayTests {
 
         Region region = Region.of("us-west-2");
         XRayClient xray = XRayClient.builder()
-            .region(region)
-            .build();
+                .region(region)
+                .build();
         BatchGetTracesResponse tracesResponse = xray.batchGetTraces(BatchGetTracesRequest.builder()
-            .traceIds(traceIds)
-            .build());
+                .traceIds(traceIds)
+                .build());
 
         // Assertions
         assertThat(tracesResponse.traces()).hasSize(traceIds.size());
-        tracesResponse.traces().forEach(trace ->
-            {
-                System.out.println(trace.id());
-                assertThat(traceIdsSet.contains(trace.id())).isTrue();
-            });
+        tracesResponse.traces().forEach(trace -> {
+            System.out.println(trace.id());
+            assertThat(traceIdsSet.contains(trace.id())).isTrue();
+        });
+    }
 
-        // Cleanup collector and OTel SDK
+    @AfterEach
+    public void resetGlobalOpenTelemetry() {
+        // Cleanup collector and otel sdk
         collector.stop();
         GlobalOpenTelemetry.resetForTest();
     }
 
     public OpenTelemetry openTelemetry(boolean useXRayIDGenerator) {
-        Resource resource = Resource.getDefault().toBuilder().put(ResourceAttributes.SERVICE_NAME, "xray-test").put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
-    
+        Resource resource = Resource.getDefault().toBuilder().put(ResourceAttributes.SERVICE_NAME, "xray-test")
+                .put(ResourceAttributes.SERVICE_VERSION, "0.1.0").build();
+
         SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-            .setSampler(Sampler.alwaysOn())
-            .setResource(resource)
-            .build();
+                .setSampler(Sampler.alwaysOn())
+                .setResource(resource)
+                .build();
 
         String exporter = System.getenv().getOrDefault("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4317");
 
         SdkTracerProvider tracerProvider;
-        
+
         if (useXRayIDGenerator) {
             tracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(
-                    BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().setEndpoint(exporter).build()).build())
-                .setIdGenerator(AwsXrayIdGenerator.getInstance())
-                .setResource(resource)
-                .build();
+                    .addSpanProcessor(
+                            BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().setEndpoint(exporter).build())
+                                    .build())
+                    .setIdGenerator(AwsXrayIdGenerator.getInstance())
+                    .setResource(resource)
+                    .build();
         } else {
             tracerProvider = SdkTracerProvider.builder()
-                .addSpanProcessor(
-                    BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().setEndpoint(exporter).build()).build())
-                .setResource(resource)
-                .build();
+                    .addSpanProcessor(
+                            BatchSpanProcessor.builder(OtlpGrpcSpanExporter.builder().setEndpoint(exporter).build())
+                                    .build())
+                    .setResource(resource)
+                    .build();
         }
-        
-        OpenTelemetry openTelemetry =
-            OpenTelemetrySdk.builder()
+
+        OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
                 .setTracerProvider(tracerProvider)
                 .buildAndRegisterGlobal();
-  
+
         return openTelemetry;
     }
 }
